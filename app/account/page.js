ef9b3f873f425723088b0aa192a73eeac41c2af2
suman kumar <sumancsnit@gmail.com>
2024-12-01 18:53:23 +0530

    add font and favicon

---
+++ app/account/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'Guest area',
+};
+
+const Page = () => {
+  return (
+    <div>
+      <h1> Your Account</h1>
+    </div>
+  );
+};
+
+export default Page;
---
+++ app/cabins/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'Cabins',
+};
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Cabin</h1>
+    </div>
+  );
+};
+
+export default Page;
