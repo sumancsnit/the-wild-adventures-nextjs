ff6e72f3f7b3bebed1f90656b3e1ec3359135c26
suman kumar <sumancsnit@gmail.com>
2024-12-04 23:02:15 +0530

    add: suspense

---
+++ app/_components/CabinList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import CabinCard from '@/app/_components/CabinCard';
+import { getCabin, getCabins } from '../_lib/data-service';
+
+const CabinList = async () => {
+  const cabins = await getCabins();
+
+  if (!cabins?.length) return null;
+  return (
+    <div>
+      <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14'>
+        {cabins.map((cabin) => (
+          <CabinCard cabin={cabin} key={cabin.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CabinList;
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,12 @@
-import CabinCard from '@/app/_components/CabinCard';
-import { getCabin, getCabins } from '../_lib/data-service';
+import { Suspense } from 'react';
+import CabinList from '../_components/CabinList';
+import Spinner from '../_components/Spinner';
+
 export const metadata = {
   title: 'Cabins',
 };
 
-export default async function Page() {
-  const cabins = await getCabins();
-  console.log('🚀 ~ Page ~ cabins:', cabins);
-  // CHANGE
-
+const Page = () => {
   return (
     <div>
       <h1 className='text-4xl mb-5 text-accent-400 font-medium'>
@@ -23,13 +21,11 @@ export default async function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins?.length > 0 && (
-        <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14'>
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
-}
+};
+
+export default Page;
