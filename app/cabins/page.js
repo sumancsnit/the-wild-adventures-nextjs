8130e6b65e5fd79b3947a116a25e4f038905d2f4
suman kumar <sumancsnit@gmail.com>
2025-01-01 16:55:24 +0530

    reservation

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import CabinList from '../_components/CabinList';
 import Spinner from '../_components/Spinner';
 import Filter from '../_components/Filter';
+import ReservationReminder from '../_components/ReservationReminder';
 
 export const revalidate = 3600;
 
@@ -31,6 +32,7 @@ export const Page = async (props) => {
 
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
