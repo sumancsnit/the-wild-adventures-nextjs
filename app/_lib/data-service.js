fca3b81f7a1755895ee986eb6ce3dfa928b1a8e6
suman kumar <sumancsnit@gmail.com>
2025-01-12 15:45:46 +0530

    Resevation form submit

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -160,6 +160,7 @@ export async function createGuest(newGuest) {
   return data;
 }
 
+/*
 export async function createBooking(newBooking) {
   const { data, error } = await supabase
     .from('bookings')
@@ -179,7 +180,7 @@ export async function createBooking(newBooking) {
 /////////////
 // UPDATE
 
-/*
+
 
 // The updatedFields is an object which should ONLY contain the updated data
 export async function updateGuest(id, updatedFields) {
